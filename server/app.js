d1c5de5e93441e625bca5cf46c789a07b0c47833
Dalton Scharff <dalton.scharff@gmail.com>
2020-05-19 21:38:39 -0500

    moves database functions to utility file

--- server/app.js
+++ server/app.js
@@ -1,29 +1,27 @@
-const { Pool } = require('pg');
 const express = require('express');
 const http = require('http');
 const SocketIo = require('socket.io');
+
+const Db = require('./utils/db');
 const Scraper = require('./utils/scraper');
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-const init = async (pool, date) => {
+const init = async (db, date) => {
     date = date || new Date;
-    let sql = 'SELECT id FROM days WHERE date=$1';
-    let result = await pool.query(sql, [date]);
+
+    // let rows = await db.readGame(date);
+    let rows = [];
 
     let dayId;
-    let answers = [];
-    let letters = [];
-    let centerLetter = '';
+    let answers;
+    let letters;
+    let centerLetter;
 
-    if (result.rowCount === 0) {
-        console.log('scraping');
-        sql = 'INSERT INTO days (date) VALUES ($1) RETURNING id';
-        result = await pool.query(sql, [date]);
-        dayId = result.rows[0].id;
-
+    if (rows.length === 0) {
+        console.log('scraping')
         const scraper = new Scraper();
         const scrapedData = await scraper.scrape();
 
@@ -31,73 +29,18 @@ const init = async (pool, date) => {
         letters = scrapedData.letters;
         centerLetter = scrapedData.centerLetter;
 
-        writeAnswers(scrapedData.answers, pool, dayId);
-        writeLetters(scrapedData.letters, pool, dayId);
-        writeCenterLetter(scrapedData.centerLetter, pool, dayId);
+        dayId = await db.writeDay(date, letters, centerLetter);
+        // await db.writeAnswers(answers, dayId);
+
     } else {
         console.log('reading');
-        dayId = result.rows[0].id;
-        answers = readAnswers(pool, dayId);
-        letters = readLetters(pool, dayId);
-        centerLetter = readCenterLetter(pool, dayId);
+        dayId = rows[0].day_id;
+        answers = rows.map((row) => row.word);
+        letters = rows[0].letters;
+        centerLetter = rows[0].center_letter;
     }
 
-    [answers, letters, centerLetter] = await Promise.all([answers, letters, centerLetter]);
     return { dayId, answers, letters, centerLetter };
-};
-
-const writeAnswers = async (answers, pool, dayId) => {
-    let values = answers.map((_, i) => `($${i + 1})`.join(','));
-    let sql = `INSERT INTO words (word) VALUES ${values} RETURNING id ON CONFLICT DO NOTHING`;
-    let result = await pool.query(sql, [...answers]);
-
-    let wordIds = result.rows.map((row) => row.id);
-
-    values = wordIds.map((_, i) => `($${i + 1}, ${wordIds.length + 1})`);
-    sql = `INSERT INTO words_to_days (word_id, day_id) VALUES ${values}`;
-    result = await pool.query(sql, [...wordIds, dayId]);
-
-    return result.rows.map((row) => row.id);
-};
-const writeLetters = async (letters, pool, dayId) => {
-    let sql = 'INSERT INTO days (letters) VALUES ($1) WHERE id=$2 RETURNING id';
-    let result = await pool.query(sql, [JSON.stringify(letters), dayId]);
-    return result.rows[0].id;
-};
-const writeCenterLetter = async (centerLetter, pool, dayId) => {
-    let sql = 'INSERT INTO days (center_letter) VALUES ($1) WHERE id=$2 RETURNING id';
-    let result = await pool.query(sql, [centerLetter, dayId]);
-    return result.rows[0].id;
-};
-const writeFoundWord = async (word, name, pool, dayId, roomId) => {
-    let sql = 'SELECT id FROM words WHERE word=$1';
-    let result = await pool.query(sql, [word]);
-    let wordId = result.rows[0].id;
-
-    sql = 'INSERT INTO words_to_rooms (word_id, room_id, day_id, player_name) VALUES ($1, $2, $3, $4) RETURNING id';
-    result = await pool.query(sql, [wordId, roomId, dayId, name]);
-    return result.rows[0].id;
-};
-
-const readAnswers = async (pool, dayId) => {
-    let sql = `SELECT word, definition FROM game_view WHERE day_id=$1`;
-    let result = await pool.query(sql, [dayId]);
-    return result.rows;
-};
-const readLetters = async (pool, dayId) => {
-    let sql = 'SELECT letters FROM days WHERE id=$1';
-    let result = await pool.query(sql, [dayId]);
-    return result.rows[0].letters;
-};
-const readCenterLetter = async (pool, dayId) => {
-    let sql = 'SELECT center_letter FROM days WHERE id=$1';
-    let result = await pool.query(sql, [dayId]);
-    return result.rows[0].center_letter;
-};
-const readFoundWords = async (pool, dayId, roomId) => {
-    let sql = 'SELECT word, definition, player_name FROM words_to_rooms JOIN words ON word_id=words.id WHERE day_id=$1 AND room_id=$2';
-    let result = await pool.query(sql, [dayId, roomId]);
-    return result.rows;
 };
 
 const getAnswerLengths = (answers) => {
@@ -133,12 +76,10 @@ const checkInWordList = (word, answers) => {
 };
 
 (async () => {
-    const pool = new Pool({
-        connectionString: process.env.DB_CONNECTION_STRING,
-        debug: process.env.NODE_ENV !== 'production',
-        max: 3
-    });
-    ({ dayId, answers, letters, centerLetter } = await init(pool));
+    const db = new Db();
+    ({ dayId, answers, letters, centerLetter } = await init(db));
+
+    console.log({ dayId, answers, letters, centerLetter })
 
     const app = express();
     const server = http.createServer(app);
@@ -183,5 +124,5 @@ const checkInWordList = (word, answers) => {
 
     server.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
 
-    await pool.end();
+    await db.disconnect();
 })();
---
+++ server/utils/db.js
@@ -0,0 +1,97 @@
+const { Pool } = require('pg');
+
+class Db {
+    constructor() {
+        this.pool = new Pool({
+            connectionString: process.env.DB_CONNECTION_STRING,
+            debug: process.env.NODE_ENV !== 'production',
+            max: 3
+        });
+    }
+
+    async disconnect() {
+        return await this.pool.end();
+    }
+
+    async writeDay(date, letters, centerLetter) {
+        let sql = 'INSERT INTO days (date, letters, center_letter) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING RETURNING id';
+        let result = await this.pool.query(sql, [date, JSON.stringify(letters), centerLetter]);
+
+        try {
+            return result.rows[0].id;
+        } catch (e) {
+            sql = 'SELECT id FROM days WHERE date=$1';
+            result = await this.pool.query(sql, [date]);
+            return result.rows[0].id;
+        }
+    }
+
+    async writeAnswers(answers, dayId) {
+        let values = answers.map((_, i) => `($${i + 1})`.join(','));
+        let sql = `INSERT INTO words (word) VALUES ${values} RETURNING id ON CONFLICT DO NOTHING`;
+        let result = await this.pool.query(sql, [...answers]);
+
+        let wordIds = result.rows.map((row) => row.id);
+
+        values = wordIds.map((_, i) => `($${i + 1}, ${wordIds.length + 1})`);
+        sql = `INSERT INTO words_to_days (word_id, day_id) VALUES ${values}`;
+        result = await this.pool.query(sql, [...wordIds, dayId]);
+
+        return result.rows.map((row) => row.id);
+    }
+
+    async writeLetters(letters, dayId) {
+        let sql = 'INSERT INTO days (letters) VALUES ($1) WHERE id=$2 RETURNING id';
+        let result = await this.pool.query(sql, [JSON.stringify(letters), dayId]);
+        return result.rows[0].id;
+    }
+
+    async writeCenterLetter(centerLetter, dayId) {
+        let sql = 'INSERT INTO days (center_letter) VALUES ($1) WHERE id=$2 RETURNING id';
+        let result = await this.pool.query(sql, [centerLetter, dayId]);
+        return result.rows[0].id;
+    }
+
+    async writeFoundWord(word, name, dayId, roomId) {
+        let sql = 'SELECT id FROM words WHERE word=$1';
+        let result = await this.pool.query(sql, [word]);
+        let wordId = result.rows[0].id;
+
+        sql = 'INSERT INTO words_to_rooms (word_id, room_id, day_id, player_name) VALUES ($1, $2, $3, $4) RETURNING id';
+        result = await this.pool.query(sql, [wordId, roomId, dayId, name]);
+        return result.rows[0].id;
+    }
+
+    async readGame(date) {
+        let sql = 'SELECT * FROM game_view WHERE date=$1';
+        let result = await this.pool.query(sql, [date]);
+        return result.rows;
+    }
+
+    async readAnswers(dayId) {
+        let sql = `SELECT word, definition FROM game_view WHERE day_id=$1`;
+        let result = await this.pool.query(sql, [dayId]);
+        return result.rows;
+    }
+
+    async readLetters(dayId) {
+        let sql = 'SELECT letters FROM days WHERE id=$1';
+        let result = await this.pool.query(sql, [dayId]);
+        return result.rows[0].letters;
+    }
+
+    async readCenterLetter(dayId) {
+        let sql = 'SELECT center_letter FROM days WHERE id=$1';
+        let result = await this.pool.query(sql, [dayId]);
+        return result.rows[0].center_letter;
+    }
+
+    async readFoundWords(dayId, roomId) {
+        let sql = 'SELECT word, definition, player_name FROM words_to_rooms JOIN words ON word_id=words.id WHERE day_id=$1 AND room_id=$2';
+        let result = await this.pool.query(sql, [dayId, roomId]);
+        return result.rows;
+    }
+}
+
+
+module.exports = Db;
