15d98e74bb86381ab561fde059f36ddb5bcba71b
Dalton Scharff <dalton.scharff@gmail.com>
2021-10-03 21:48:24 -0500

    adds words endpoint

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,4 @@
-import { connect, Room } from "spelling-bee-core";
+import { connect, Room, Word } from "spelling-bee-core";
 import fastify from "fastify";
 
 const server = fastify({});
@@ -11,16 +11,22 @@ server.get('/rooms', async (request, reply) => {
     return Room.find();
 });
 
+server.get('/words', async (request, reply) => {
+    return Word.find();
+})
+
 connect({
     url: process.env.DATABASE_URL,
-    logging: true
+    logging: process.env.NODE_ENV === "development"
 })
     .then(async () => {
         try {
-            await server.listen(3000);
-            console.log("Server started on http://localhost:3000");
-        } catch (err) {
-            server.log.error(err);
+            const port = process.env.APP_PORT || 3000;
+            const host = process.env.APP_HOST || "localhost"
+            await server.listen(port, host);
+            console.log(`Server started on http://${host}:${port}`);
+        } catch (error) {
+            server.log.error(error);
             process.exit(1);
         }
     })
