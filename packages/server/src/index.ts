39fa2d2fa7a33f06adac4f9bde9f7c837ca6c2c5
Dalton Scharff <dalton.scharff@gmail.com>
2021-10-23 16:03:23 -0500

    adds socket.io example

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -7,23 +7,38 @@ const port = process.env.APP_PORT || 3000;
 const host = process.env.APP_HOST || "localhost";
 
 async function main() {
-  const promises = [];
-
-  promises.push(connect({
+  const dbPromise = connect({
     url: process.env.DATABASE_URL,
     logging: process.env.NODE_ENV === "development",
-  }))
+  });
 
-  server.register(socketio);
-  promises.push(server.listen(port, host));
+  server.register(socketio, {
+    cors: { origin: "*", methods: ["GET", "POST"] },
+  });
 
   try {
-    await Promise.all(promises);
+    await server.ready();
+  } catch (error) {
+    server.log.error(error);
+    process.exit(1);
+  }
+  server.io.on("connection", onConnection);
+
+  const fastifyPromise = server.listen(port, host);
+
+  try {
+    await Promise.all([dbPromise, fastifyPromise]);
     console.log(`Server started on http://${host}:${port}`);
   } catch (error) {
     server.log.error(error);
     process.exit(1);
   }
+}
+
+function onConnection(socket) {
+  socket.on("hello", (payload) => {
+    console.log(payload, "HELLO");
+  });
 }
 
 main();
