7dfeb221dc65af626749e64b8088e6d3b4a6f6f4
Dalton Scharff <dalton.scharff@gmail.com>
2021-10-24 16:03:52 -0500

    renames MyApp to App

--- packages/web-client/pages/_app.tsx
+++ packages/web-client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "semantic-ui-css/semantic.min.css";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-export default MyApp;
+export default App;
