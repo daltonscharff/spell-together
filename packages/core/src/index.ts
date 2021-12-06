1a9aadef3539c1d4c69e0f6616981e57d4ce3335
Dalton Scharff <dalton.scharff@gmail.com>
2021-12-05 20:13:55 -0600

    Removes puzzleWord, assuming only one puzzle stored

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,7 +6,6 @@ import {
   Record,
   Puzzle,
   RoomWord,
-  PuzzleWord,
 } from "@prisma/client";
 import generateRoomCode from "./generateRoomCode";
 
@@ -18,10 +17,20 @@ prisma.$use(async (params: Prisma.MiddlewareParams, next) => {
     params.model === "Room" &&
     params.args.data.code === null
   ) {
-    let room = params.args.data;
+    const room = params.args.data;
     room.code = generateRoomCode(6);
   }
+
+  // if (
+  //   params.action === "create" &&
+  //   params.model === "Word" &&
+  //   params.args.data?.word
+  // ) {
+  //   let word = params.args.data.word;
+  //   word = word.toLowerCase();
+  // }
+
   return await next(params);
 });
 
-export { Word, Room, Record, Puzzle, RoomWord, PuzzleWord };
+export { Word, Room, Record, Puzzle, RoomWord };
