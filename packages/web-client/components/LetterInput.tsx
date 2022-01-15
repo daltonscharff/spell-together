75683710b989a3048eb4bb7a71db9e80f5a7de15
Dalton Scharff <dalton.scharff@gmail.com>
2022-01-14 21:44:22 -0600

    fixes letter input to remove input tag

--- packages/web-client/components/LetterInput.tsx
+++ packages/web-client/components/LetterInput.tsx
@@ -1,11 +1,10 @@
-import { FC, RefObject, useRef, useState } from "react";
+import { FC, RefObject, useEffect, useRef, useState } from "react";
 
 type Props = {
   value: string;
   validLetters: string[];
   centerLetter: string;
-  inputRef: RefObject<HTMLInputElement>;
-  onChange: (value: string) => void;
+  onChange: (key: string) => void;
 };
 
 type LetterCategory = "valid" | "invalid" | "center";
@@ -26,52 +25,68 @@ const LetterInput: FC<Props> = ({
   value,
   validLetters,
   centerLetter,
-  inputRef,
   onChange,
 }) => {
-  const [inputFocus, setInputFocus] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  useEffect(() => {
+    const registerEvent = (event: KeyboardEvent) => {
+      onChange(event.key);
+    };
+
+    window.addEventListener("keydown", registerEvent);
+    return () => {
+      window.removeEventListener("keydown", registerEvent);
+    };
+  }, [onChange]);
+
+  useEffect(() => {
+    const handleInFocus = () => setIsFocused(true);
+    const handleOutFocus = () => setIsFocused(false);
+    window.addEventListener("focus", handleInFocus);
+    window.addEventListener("blur", handleOutFocus);
+    return () => {
+      window.removeEventListener("focus", handleInFocus);
+      window.removeEventListener("blur", handleOutFocus);
+    };
+  }, []);
+
   return (
-    <>
-      <div className="font-black text-3xl text-center py-3 flex flex-wrap justify-center">
-        {value.split("").map((letter, i) => {
-          const category = checkLetterCategory(
-            letter,
-            validLetters,
-            centerLetter
-          );
-          let color = "";
-          switch (category) {
-            case "center":
-              color = "text-yellow-300";
-              break;
-            case "valid":
-              color = "text-black";
-              break;
-            default:
-              color = "text-zinc-300";
-          }
-          return (
-            <span key={i} className={`${color}`}>
-              {letter}
-            </span>
-          );
-        })}
-        {inputFocus && (
-          <div className="animate-blink mx-[2px] h-9 w-[2px] bg-yellow-300" />
-        )}
-      </div>
-      <input
-        type="text"
-        className="focus:outline-none h-0"
-        placeholder={"Type..."}
-        value={value}
-        maxLength={20}
-        onChange={(event) => onChange(event.target.value)}
-        onFocus={() => !inputFocus && setInputFocus(true)}
-        onBlur={() => setInputFocus(false)}
-        ref={inputRef}
+    <div className="font-black text-3xl text-center py-3 flex flex-wrap justify-center">
+      {value.split("").map((letter, i) => {
+        const category = checkLetterCategory(
+          letter,
+          validLetters,
+          centerLetter
+        );
+        let color = "";
+        switch (category) {
+          case "center":
+            color = "text-yellow-300";
+            break;
+          case "valid":
+            color = "text-black";
+            break;
+          default:
+            color = "text-zinc-300";
+        }
+        return (
+          <span key={i} className={`${color}`}>
+            {letter}
+          </span>
+        );
+      })}
+
+      <div
+        className={`animate-blink mx-[2px] h-9 w-[2px] bg-yellow-300 ${
+          !isFocused && "invisible"
+        }`}
       />
-    </>
+
+      {!isFocused && value.length === 0 && (
+        <div className="font-normal text-zinc-300">Type or click...</div>
+      )}
+    </div>
   );
 };
 
