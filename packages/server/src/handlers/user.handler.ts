d3c893f4d4f1006b8794e5f50f8695eb82424cc4
Dalton Scharff <dalton.scharff@gmail.com>
2022-01-31 20:39:26 -0600

    adds onGuess functionality to user handler

--- packages/server/src/handlers/user.handler.ts
+++ packages/server/src/handlers/user.handler.ts
@@ -1,23 +1,39 @@
 import type { Socket } from "socket.io";
 import { Guess, User } from "../interfaces";
+import { recordService } from "../services";
 
 export function onGuess(guess: Guess) {
   const socket: Socket = this;
-  console.log("hello from onGuess");
-  socket.emit("abc", "from onGuess");
+  const shortcode = guess.shortcode;
+  const record = recordService.create(
+    guess.word,
+    guess.shortcode,
+    guess.username
+  );
+  if (record) {
+    socket.emit("correctGuess", guess);
+    socket
+      .to(shortcode)
+      .emit("updateFoundWords", recordService.findAllInRoom(shortcode, "word"));
+  } else {
+    socket.emit("incorrectGuess", guess);
+  }
 }
 
 export function onJoinRoom(user: User) {
   const socket: Socket = this;
-  const shortcode = user.shortcode.toLowerCase();
+  const shortcode = user.shortcode;
   socket.join(shortcode);
-  console.log("joining room:", shortcode);
+  socket.emit(
+    "updateFoundWords",
+    recordService.findAllInRoom(shortcode, "word")
+  );
   // socket.broadcast.to(shortcode).emit("room:userJoined", user.username);
 }
 
 export function onLeaveRoom(user: User) {
   const socket: Socket = this;
-  const shortcode = user.shortcode.toLowerCase();
+  const shortcode = user.shortcode;
   socket.leave(shortcode);
 }
 
