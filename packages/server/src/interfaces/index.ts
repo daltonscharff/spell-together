9b528db1ab5ab48d55e903e556a76b6f213e7264
Dalton Scharff <dalton.scharff@gmail.com>
2022-02-03 11:59:56 -0600

    removes CorrectGuess from index

--- packages/server/src/interfaces/index.ts
+++ packages/server/src/interfaces/index.ts
@@ -2,6 +2,6 @@ import type Puzzle from "./puzzle.interface";
 import type Record from "./record.interface";
 import type Room from "./room.interface";
 import type Word from "./word.interface";
-import type { User, Guess, CorrectGuess } from "./user.interface";
+import type { User, Guess } from "./user.interface";
 
-export { Puzzle, Record, Room, Word, User, Guess, CorrectGuess };
+export { Puzzle, Record, Room, Word, User, Guess };
