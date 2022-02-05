12921c58bd628d95fedd6fbd8a952a6c59f3c179
Dalton Scharff <dalton.scharff@gmail.com>
2022-02-05 13:16:30 -0600

    fixes fetcher env variable

--- packages/web/utils/fetcher.ts
+++ packages/web/utils/fetcher.ts
@@ -1,5 +1,9 @@
 export default async function fetcher(endpoint: string) {
-  const base = process.env.NEXT_PUBLIC_SOCKET_URL || "";
-  const res = await fetch(new URL(endpoint, base).href);
-  return res.json();
+  const base = process.env.NEXT_PUBLIC_SERVER_URL || "";
+  try {
+    const res = await fetch(new URL(endpoint, base).href);
+    return res.json();
+  } catch (e) {
+    console.log(e);
+  }
 }
