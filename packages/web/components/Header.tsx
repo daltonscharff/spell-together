eef4929134080b145bcea548679a76051b39750a
Dalton Scharff <dalton.scharff@gmail.com>
2022-02-06 12:15:04 -0600

    header stops expanding at large size

--- packages/web/components/Header.tsx
+++ packages/web/components/Header.tsx
@@ -8,14 +8,16 @@ type Props = {
 
 const Header: FC<Props> = ({ date, username }) => {
   return (
-    <header className="py-4 px-3 border-b border-zinc-300 flex flex-row items-end">
-      <div className="flex flex-col sm:gap-4 sm:flex-row pr-4 flex-grow sm:items-end">
-        <div className="font-RobotoSlab text-3xl">Spell Together</div>
-        <div className="font-light text-xl">
-          {dayjs(date).format("MMMM D, YYYY")}
+    <header className="border-b border-zinc-300">
+      <div className="max-w-screen-lg mx-auto flex flex-row items-end py-4 px-3">
+        <div className="flex flex-col sm:gap-4 sm:flex-row pr-4 flex-grow sm:items-end">
+          <div className="font-RobotoSlab text-3xl">Spell Together</div>
+          <div className="font-light text-xl">
+            {dayjs(date).format("MMMM D, YYYY")}
+          </div>
         </div>
+        {username && <div className="font-light">{username}</div>}
       </div>
-      {username && <div className="font-light">{username}</div>}
     </header>
   );
 };
