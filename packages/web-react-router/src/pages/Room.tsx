e2dfc883e868c7057a8fc24be9dfd8de0f64cd7a
Dalton Scharff <dalton.scharff@gmail.com>
2022-03-06 16:36:44 -0600

    loads room and puzzle data in Room

--- packages/web-react-router/src/pages/Room.tsx
+++ packages/web-react-router/src/pages/Room.tsx
@@ -1,11 +1,27 @@
 import { useParams } from "react-router-dom";
 import { useStore } from "../store";
+import useSWR from "swr";
+import fetcher from "../utils/fetcher";
 
 export function Room() {
   let params = useParams();
   const setShortcode = useStore((state) => state.setShortcode);
   if (params.shortcode) setShortcode(params.shortcode);
   const shortcode = useStore((state) => state.shortcode);
+
+  const { data: puzzleData, error: puzzleError } = useSWR(
+    "api/puzzles/newest",
+    fetcher
+  );
+
+  console.log({ puzzleData, puzzleError });
+
+  const { data: roomData, error: roomError } = useSWR(
+    `api/rooms/${shortcode}`,
+    fetcher
+  );
+
+  console.log({ roomData, roomError });
 
   return <>Room {shortcode}</>;
 }
