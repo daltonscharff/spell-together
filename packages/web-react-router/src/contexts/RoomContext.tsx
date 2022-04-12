b3b6903e2ab213ab95b3054baaf127d4e633059b
Dalton Scharff <dalton.scharff@gmail.com>
2022-04-11 20:28:47 -0500

    update RoomContext

--- packages/web-react-router/src/contexts/RoomContext.tsx
+++ packages/web-react-router/src/contexts/RoomContext.tsx
@@ -1,14 +1,43 @@
-import { createContext, PropsWithChildren, useState } from "react";
+import { createContext, PropsWithChildren, useContext, useState } from "react";
 
 export type RoomContextProps = PropsWithChildren<{}>;
 
-export const RoomContext = createContext([{}, () => {}]);
+export type RoomType = {
+  shortcode: string | null;
+  name: string | null;
+};
+
+const nullRoom: RoomType = {
+  shortcode: null,
+  name: null,
+};
+
+export const RoomContext = createContext<[RoomType, (state: RoomType) => void]>(
+  [nullRoom, () => {}]
+);
 
 export const RoomProvider = (props: RoomContextProps) => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState<RoomType>({
+    ...nullRoom,
+    shortcode: localStorage.getItem("shortcode"),
+  });
   return (
     <RoomContext.Provider value={[state, setState]}>
       {props.children}
     </RoomContext.Provider>
   );
+};
+
+export const useRoom = () => {
+  const [state, setState] = useContext(RoomContext);
+
+  function setShortcode(shortcode: string) {
+    localStorage.setItem("shortcode", shortcode);
+    setState({ ...state, shortcode });
+  }
+
+  return {
+    ...state,
+    setShortcode,
+  };
 };
