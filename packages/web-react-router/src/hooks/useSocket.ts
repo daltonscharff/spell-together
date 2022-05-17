3e0dfa81f1af7273c805576def068778526d5305
Dalton Scharff <dalton.scharff@gmail.com>
2022-05-16 21:33:36 -0500

    updates useSocket URL

--- packages/web-react-router/src/hooks/useSocket.ts
+++ packages/web-react-router/src/hooks/useSocket.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socketUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+const socketUrl = process.env.REACT_APP_SERVER_URL;
 export const socket = io(socketUrl!);
 
 socket.on("error", (error) => console.log(error));
@@ -21,5 +21,3 @@ export function useSocket(eventName: string, cb: (...args: any[]) => void) {
 
   return socket;
 }
-
-export default useSocket;
