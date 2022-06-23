6b5d4784f1c365910d93d38192265b59cc2dc0af
Dalton Scharff <dalton.scharff@gmail.com>
2022-06-22 20:15:43 -0500

    removes themes and simplifies index.tsx

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,34 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { UserProvider } from "./contexts/UserContext";
 import Home from "./pages/home";
-import Room from "./pages/rooms/room";
-import { CreateRoom } from "./pages/rooms/create";
-import { JoinRoom } from "./pages/rooms/join";
-import { ThemeProvider } from "@mui/material/styles";
-import { lightTheme } from "./themes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <UserProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/rooms">
-              <Route path=":shortcode" element={<Room />} />
-              <Route path="create" element={<CreateRoom />} />
-              <Route path="join" element={<JoinRoom />} />
-            </Route>
-            <Route path="*" element={<div>Page not found</div>} />
-          </Routes>
-        </BrowserRouter>
-      </UserProvider>
-    </ThemeProvider>
+    <Home />
   </React.StrictMode>
 );
