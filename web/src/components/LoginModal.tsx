75bc7bab9f4aa9814db1cb62898398aab68d933c
Dalton Scharff <dalton.scharff@gmail.com>
2022-06-26 19:14:40 -0500

    styles tabs on login modal

--- web/src/components/LoginModal.tsx
+++ web/src/components/LoginModal.tsx
@@ -5,26 +5,34 @@ export const LoginModal = () => {
     <Dialog open={true} onClose={() => {}}>
       <div className="fixed inset-0 bg-black/5" aria-hidden="true" />
       <div className="fixed inset-0 flex flex-col items-center justify-center p-4">
-        <Tab.Group>
-          <Tab.List>
-            <Tab>Login</Tab>
-            <Tab>Create Room</Tab>
-          </Tab.List>
-          <Tab.Panels>
-            <Tab.Panel>
-              <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
+        <Dialog.Panel className="mx-auto max-w-md w-full rounded bg-white p-4 shadow-md">
+          <Tab.Group>
+            <Tab.List className="flex border rounded mb-2 bg-zinc-100 text-zinc-600">
+              {["Login", "Create"].map((tab) => (
+                <Tab
+                  key={tab}
+                  className={({ selected }) =>
+                    `w-full text-center rounded py-1 px-4 ${
+                      selected && "bg-white text-black shadow-sm"
+                    }`
+                  }
+                >
+                  {tab}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel>
                 <Dialog.Title>Login</Dialog.Title>
                 <p>You can login here</p>
-              </Dialog.Panel>
-            </Tab.Panel>
-            <Tab.Panel>
-              <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
-                <Dialog.Title>Create Room</Dialog.Title>
+              </Tab.Panel>
+              <Tab.Panel>
+                <Dialog.Title>Create</Dialog.Title>
                 <p>You can create a room here</p>
-              </Dialog.Panel>
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </Dialog.Panel>
       </div>
     </Dialog>
   );
