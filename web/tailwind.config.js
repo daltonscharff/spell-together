645c4756e5d255999a02337f303f63c6cc7f8bb5
Dalton Scharff <dalton.scharff@gmail.com>
2022-07-04 21:49:52 -0500

    limits container to 1024px

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding: "1rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+      },
     },
     extend: {
       fontFamily: {
