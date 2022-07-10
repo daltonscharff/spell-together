7fefd9721d5481892ec750d6700a6c3c1a6bf510
Dalton Scharff <dalton.scharff@gmail.com>
2022-07-10 15:05:22 -0500

    adds separate gameroom page

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,18 +1,8 @@
 import { LetterInputProvider } from "../contexts/LetterInputContext";
-import { useCorrectGuesses } from "../hooks/useCorrectGuesses";
-import { usePuzzle } from "../hooks/usePuzzle";
-import { useRoom } from "../hooks/useRoom";
 import { useShortcode } from "../hooks/useShortcode";
 import { useUsername } from "../hooks/useUsername";
-import { ButtonArea } from "./ButtonArea";
-import { CorrectGuessList } from "./CorrectGuessList";
-import { Footer } from "./Footer";
-import { FoundWordDisplay } from "./FoundWordDisplay";
-import { Header } from "./Header";
-import { Hive } from "./Hive";
-import { LetterInput } from "./LetterInput";
+import { GameRoom } from "../pages/GameRoom";
 import { LoginModal } from "./LoginModal";
-import { PointDisplay } from "./PointDisplay";
 
 export const App = () => {
   const { username } = useUsername();
@@ -30,28 +20,7 @@ export const App = () => {
 
   return (
     <LetterInputProvider>
-      <div className="flex flex-col gap-y-8 min-h-screen">
-        <Header />
-        <div className="container grid gap-8 grid-cols-1 md:grid-cols-2 flex-grow">
-          <div className="flex flex-col gap-8 mx-auto max-w-sm min-w-[200px]">
-            <LetterInput />
-            <Hive />
-            <ButtonArea />
-          </div>
-          <div className="flex flex-col mx-auto max-w-lg min-w-[200px] border-black border rounded-sm md:max-h-[600px]">
-            <div className="p-4 border-b-2 border-black">
-              <PointDisplay />
-            </div>
-            <div className="p-4 border-b-2 border-black">
-              <FoundWordDisplay />
-            </div>
-            <div className="p-2 overflow-y-auto">
-              <CorrectGuessList />
-            </div>
-          </div>
-        </div>
-        <Footer />
-      </div>
+      <GameRoom />
     </LetterInputProvider>
   );
 };
