66134613ee4673269adf9e05f2e372da0e6de14c
Dalton Scharff <dalton.scharff@gmail.com>
2022-08-01 13:17:32 -0500

    underline links on hover

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -6,17 +6,17 @@ export const Footer = () => {
     <div className="container text-center text-sm font-light text-zinc-300 border-t border-zinc-200 pt-3 my-4">
       {puzzle?.editor && <p>Puzzle by {puzzle?.editor}.</p>}
       <p>
-        This multiplayer version of the{" "}
+        This multiplayer version of{" "}
         <a
           href="https://www.nytimes.com/puzzles/spelling-bee"
-          className="underline"
+          className="hover:underline"
         >
-          New York Times Spelling Bee
+          The New York Times Spelling Bee
         </a>{" "}
         was created by{" "}
         <a
           href="https://github.com/daltonscharff/spell-together"
-          className="underline"
+          className="hover:underline"
         >
           Dalton Scharff
         </a>
