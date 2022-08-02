a0d5ca575ce3271725f9e545e1ae0566a2692d26
Dalton Scharff <dalton.scharff@gmail.com>
2022-08-01 20:06:44 -0500

    adds disabled to cta button

--- web/src/components/CtaButton.tsx
+++ web/src/components/CtaButton.tsx
@@ -8,7 +8,9 @@ export const CtaButton: FC<Props> = ({ children, className, ...props }) => {
   return (
     <button
       {...props}
-      className={`px-8 py-3 bg-primary border border-primary rounded-sm uppercase font-bold ${className}`}
+      className={`px-8 py-3 bg-primary border border-primary rounded-sm uppercase font-bold ${className} ${
+        props.disabled && "bg-zinc-200 text-zinc-400 border-zinc-200"
+      }`}
     >
       {children}
     </button>
