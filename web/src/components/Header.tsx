852e0ff2d2bbc5a96358e7926bb5b20448eb379a
Dalton Scharff <dalton.scharff@gmail.com>
2022-08-04 20:31:52 -0500

    fixes header layout on small screens

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -10,37 +10,28 @@ export const Header = ({ titleOnly }: Props) => {
   const { puzzle } = usePuzzle();
   const navigate = useNavigate();
 
-  const shortcode = useLocalStore((state) => state.shortcode);
+  const username = useLocalStore((state) => state.username);
   return (
-    <div className="container mt-3 border-b border-black pb-2 sm:pb-0">
-      <div className="grid grid-cols-2 sm:grid-cols-3 items-baseline gap-x-4 gap-x mb-2">
-        <div>
-          <h1
-            className="font-display text-2xl text-left col-span-2 sm:col-span-1 cursor-pointer inline"
-            onClick={() => navigate("/")}
-          >
-            Spell Together
-          </h1>
-        </div>
+    <div className={"container mt-3 border-b border-black sm:pb-0"}>
+      <div className="grid grid-cols-1 md:grid-cols-3 items-baseline gap-x-4 gap-y-1 mb-2">
+        <h1
+          className="font-display text-2xl text-center md:text-left cursor-pointer inline"
+          onClick={() => navigate("/")}
+        >
+          Spell Together
+        </h1>
         {!titleOnly && (
           <>
-            {puzzle?.date && (
-              <div className="font-light text-left sm:text-center">
-                {new Date(puzzle.date).toLocaleDateString(undefined, {
+            <div className="font-light text-center italic">
+              {puzzle?.date &&
+                new Date(puzzle?.date).toLocaleDateString(undefined, {
                   dateStyle: "full",
                 })}
-              </div>
-            )}
-            {shortcode && (
-              <div className="cursor-pointer uppercase font-semibold text-right ml-2">
-                {shortcode}
-                <img
-                  src="/icons/leave.svg"
-                  alt="Leave room"
-                  className="h-3 pl-1 relative bottom-[2px] inline-block"
-                />
-              </div>
-            )}
+            </div>
+
+            <div className="font-light text-center md:text-right ml-2">
+              {username}
+            </div>
           </>
         )}
       </div>
