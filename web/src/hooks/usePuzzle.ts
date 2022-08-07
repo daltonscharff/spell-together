3827761d2cb4c078c8f5d9cee9f44d17dfb7a5ee
Dalton Scharff <dalton.scharff@gmail.com>
2022-08-06 22:16:15 -0500

    creates view to query for newest puzzles

--- web/src/hooks/usePuzzle.ts
+++ web/src/hooks/usePuzzle.ts
@@ -5,7 +5,7 @@ import fetcher from "../utils/fetcher";
 export const usePuzzle = () => {
   const puzzlesToLoad = 1;
   const { data, error } = useSWR<Puzzle[]>(
-    `/rest/v1/puzzle?select=*&limit=${puzzlesToLoad}`,
+    `/rest/v1/newest_puzzle?select=*&limit=${puzzlesToLoad}`,
     fetcher
   );
 
