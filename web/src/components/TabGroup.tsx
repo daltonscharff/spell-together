8bbbe673bdd3da562112cfa02053c9760064b90c
Dalton Scharff <dalton.scharff@gmail.com>
2022-08-20 21:02:51 -0500

    remove extra div from tab group

--- web/src/components/TabGroup.tsx
+++ web/src/components/TabGroup.tsx
@@ -10,28 +10,26 @@ type Props = {
 
 export const TabGroup = ({ tabs }: Props) => {
   return (
-    <div>
-      <Tab.Group>
-        <Tab.List className="flex border rounded-sm mb-2 bg-zinc-100 text-zinc-600">
-          {tabs.map((tab, i) => (
-            <Tab
-              key={tab.label + i}
-              className={({ selected }) =>
-                `w-full text-center rounded-sm py-1 px-4 ${
-                  selected && "bg-white text-black shadow-sm"
-                }`
-              }
-            >
-              {tab.label}
-            </Tab>
-          ))}
-        </Tab.List>
-        <Tab.Panels>
-          {tabs.map((tab, i) => (
-            <Tab.Panel key={tab.label + i}>{tab.element}</Tab.Panel>
-          ))}
-        </Tab.Panels>
-      </Tab.Group>
-    </div>
+    <Tab.Group>
+      <Tab.List className="flex border rounded-sm mb-2 bg-zinc-100 text-zinc-600">
+        {tabs.map((tab, i) => (
+          <Tab
+            key={tab.label + i}
+            className={({ selected }) =>
+              `w-full text-center rounded-sm py-1 px-4 ${
+                selected && "bg-white text-black shadow-sm"
+              }`
+            }
+          >
+            {tab.label}
+          </Tab>
+        ))}
+      </Tab.List>
+      <Tab.Panels>
+        {tabs.map((tab, i) => (
+          <Tab.Panel key={tab.label + i}>{tab.element}</Tab.Panel>
+        ))}
+      </Tab.Panels>
+    </Tab.Group>
   );
 };
