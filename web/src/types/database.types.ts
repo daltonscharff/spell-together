ede6d9bcc6e7f1b19db0adee6725caa708f11ddc
Dalton Scharff <dalton.scharff@gmail.com>
2025-06-25 20:42:26 -0400

    add database types

---
+++ web/src/types/database.types.ts
@@ -0,0 +1,41 @@
+export type Guess = {
+  created_at: string;
+  id: string;
+  is_correct: boolean;
+  puzzle_id: string;
+  room_id: string;
+  submitted_by: string;
+  word_id: string;
+};
+
+export type Puzzle = {
+  center_letter: string;
+  created_at: string;
+  date: string;
+  id: string;
+  max_score: number;
+  outer_letters: string[];
+};
+
+export type PuzzleToWord = {
+  created_at: string;
+  id: string;
+  puzzle_id: string;
+  word_id: string;
+};
+
+export type Room = {
+  created_at: string;
+  id: string;
+  shortcode: string;
+};
+
+export type Word = {
+  created_at: string;
+  definition: string | null;
+  id: string;
+  is_pangram: boolean;
+  part_of_speech: string | null;
+  point_value: number;
+  word: string;
+};
