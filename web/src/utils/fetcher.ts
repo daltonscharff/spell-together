b679fa89232e711b3d67d0a169e14091886251bf
Dalton Scharff <dalton.scharff@gmail.com>
2025-06-26 10:11:42 -0400

    change HOST to string

--- web/src/utils/fetcher.ts
+++ web/src/utils/fetcher.ts
@@ -1,7 +1,6 @@
-const HOST = new URL("http://127.0.0.1:54321");
+const HOST = "http://127.0.0.1:54321";
 
 export const fetcher = (resource: string) => {
-  const url = `${HOST}rest/v1/${resource}`;
-  console.log(url);
+  const url = `${HOST}/rest/v1/${resource}`;
   return fetch(url).then((res) => res.json());
 };
