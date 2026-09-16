4f369cf7d2f7d8ca151eb8d8ef0a20229e970b29
Dalton Scharff <dalton.scharff@gmail.com>
2026-09-15 19:53:23 -0500

    delete everything

--- supabase/migrations/20220527000914_realtime-guess-puzzle-room.sql
+++
@@ -1,3 +0,0 @@
-alter publication supabase_realtime add table guess;
-alter publication supabase_realtime add table puzzle;
-alter publication supabase_realtime add table room;
--- web/src/utils/validateShortcode.ts
+++
@@ -1,9 +0,0 @@
-import { supabase } from "./supabaseClient";
-
-export async function validateShortcode(shortcode: string) {
-  const { count } = await supabase
-    .from("room")
-    .select("*", { count: "exact" })
-    .eq("shortcode", shortcode.toLowerCase());
-  return !!count;
-}
