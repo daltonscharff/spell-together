28791d0ad68dac6789c5db62cf254c9c660a085f
Dalton Scharff <dalton.scharff@gmail.com>
2026-09-18 16:11:39 -0500

    feat: update cron job configurations and enhance logging; streamline environment variables for better management

--- scripts/logger.ts
+++ scripts/logger.ts
@@ -1,12 +1,31 @@
 import pino from "pino";
 
-export default pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: false,
-      translateTime: "SYS:standard",
-      ignore: "pid,hostname",
-    },
-  },
-});
+const logger =
+  process.env.NODE_ENV === "production"
+    ? pino({
+        level: process.env.PINO_LOG_LEVEL || "info",
+        transport: {
+          target: "pino-roll",
+          options: {
+            frequency: "daily",
+            limit: { count: 7 },
+            mkdir: true,
+            dateFormat: "yyyy-MM-dd",
+            file: process.env.LOG_FILE_PATH,
+            prefix: "cron",
+          },
+        },
+      })
+    : pino({
+        level: process.env.PINO_LOG_LEVEL || "info",
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+            ignore: "pid,hostname",
+          },
+        },
+      });
+
+export default logger;
